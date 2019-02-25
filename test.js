2c5688c999522937ef9d54046e311bcaa600f862
Liza <lisaveta256@gmail.com>
2019-02-25 20:20:27 +0300

    My first commit

---
+++ test.js
@@ -0,0 +1,20 @@
+﻿
+'use strict';
+var money, time,list,cost;
+money=prompt("Ваш бюджет на месяц");
+time=prompt("Введите дату в формате YY-MM-DD");
+var appData={};
+appData.money=money;
+appData.timeData=time;
+appData.expenses={};
+appData.optionalExpenses={};
+appData.income=[];
+appData.saving=false;
+for(var i=1;i<3;i++){
+    list=prompt('Введите обязательную статью расходов в этом месяце');
+    cost=prompt("Во сколько это обойдётся?");
+    }
+appData.expenses={
+    list:   cost
+};
+alert (+money/30);
